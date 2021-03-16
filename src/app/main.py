a6016c457937aeab90e4a3e41d7883468ded587a
harshs14 <harsh.singh1411@gmail.com>
2021-03-16 17:27:47 +0530

    url redirect

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,12 @@
 import models
 from datetime import datetime, timezone
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 from pydantic import HttpUrl
 from hashlib import md5
 from models import UrlShortenerModel
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -18,17 +19,17 @@ def get_db():
     finally:
         db.close()
 
-def hash_url(original_url: str, timestamp: float):
+def hash_url(original_url: str, timestamp: float, db: Session = Depends(get_db)):
     data = f"{original_url}{timestamp}"
-    # print(data)
     short_url = md5(data.encode()).hexdigest()[:7]
 
-    # if UrlShortenerModel.
-
+    # obj = db.query(UrlShortenerModel).filter_by(short_url=short_url).first()
+    # if obj:
+    #    return hash_url(original_url,timestamp)
     return short_url
 
 @app.post("/api/url_shortener")
-def url_shortener(url : HttpUrl, db : Session = Depends(get_db)):
+def url_shortener(url: HttpUrl, db: Session = Depends(get_db)):
     timestamp = datetime.now().replace(tzinfo=timezone.utc).timestamp()
     short_url = hash_url(url,timestamp)
     
@@ -41,3 +42,11 @@ def url_shortener(url : HttpUrl, db : Session = Depends(get_db)):
 @app.get("/api/get_shortened_urls")
 def get_shortened_urls(db: Session = Depends(get_db)):
     return db.query(UrlShortenerModel).all()
+
+@app.get("/api/{short_url}")
+def redirect_url(short_url: str, db: Session = Depends(get_db)):
+    url_obj = db.query(UrlShortenerModel).filter_by(short_url=short_url).first()
+
+    if not url_obj:
+        raise HTTPException(status_code=404, detail="Url does not exist!!!")
+    return RedirectResponse(url = url_obj.original_url)
